af77a29d1b11e465d1237d404c4906a3dd92a469
Rolando Tactay <103474360+rtactayjr@users.noreply.github.com>
2022-11-19 21:10:17 +0800

    Create app.py

---
+++ flask weather app/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import requests # pip install requests
+
+app = Flask(__name__) #Initialise app
+
+# Config
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        city_name = request.form['name']
+
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=fb21af01f6ea5705f0a09af793dd7ae4'
+        response = requests.get(url.format(city_name)).json()
+        
+        temp = response['main']['temp']
+        weather = response['weather'][0]['description']
+        min_temp = response['main']['temp_min']
+        max_temp = response['main']['temp_max']
+        icon = response['weather'][0]['icon']
+        
+        print(temp,weather,min_temp,max_temp,icon)
+        return render_template('weather.html',temp=temp,weather=weather,min_temp=min_temp,max_temp=max_temp,icon=icon, city_name = city_name)
+    else:
+        return render_template('weather.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
